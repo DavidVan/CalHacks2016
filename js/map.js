eeff31ba2d568eaa03ae5c27f89798c34a48bf49
David Van <David@DavidVan.net>
2016-11-12 01:35:38 -0800

    Added setLocation function.

--- js/map.js
+++ js/map.js
@@ -1,5 +1,6 @@
+var map;
 function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 0, lng: 0},
         zoom: 2
     });
@@ -10,3 +11,10 @@ $(window).resize(function () {
     offset = 50;
     $('#map').css('height', (height - offset));
 }).resize();
+
+function setLocation(lat, lng) {
+    var marker = new google.maps.Marker({
+        position: {lat: lat, lng: lng}
+    });
+    marker.setMap(map);
+}
